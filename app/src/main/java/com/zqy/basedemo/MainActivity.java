c2721738ccc8e1bf16977471ef069bbefc036659
zhaoqy <zhaoqy@careland.com.cn>
2018-05-15 16:23:56 +0800

    update

--- app/src/main/java/com/zqy/basedemo/MainActivity.java
+++ app/src/main/java/com/zqy/basedemo/MainActivity.java
@@ -1,8 +1,21 @@
 package com.zqy.basedemo;
 
+import android.widget.TextView;
+
 import com.zqy.basedemo.base.BaseActivity;
 
+import java.math.BigDecimal;
+
+import butterknife.BindView;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tv_text0)
+    TextView textView0;
+
+    @BindView(R.id.tv_text1)
+    TextView textView1;
+
 
     @Override
     protected int getLayoutResID() {
@@ -11,7 +24,28 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        double dis0 = 1852.22;
+        double dis1 = 52.22;
 
+        textView0.setText("textView0" + getDistance(dis0));
+        textView1.setText("textView1" + getDistance(dis1));
+    }
+
+    private String getDistance(double dis) {
+        String distance = "";
+        if (dis >= 1000) {
+            BigDecimal bg = new BigDecimal(dis / 1000.0);
+            double f1 = bg.setScale(1, BigDecimal.ROUND_HALF_UP).doubleValue();
+            distance = " (差距" + getPrettyNumber(f1) + "公里)";
+        } else {
+            distance = " (差距" + (int)dis + "米)";
+        }
+        return distance;
+    }
+
+    public static String getPrettyNumber(double number) {
+        return BigDecimal.valueOf(number)
+                .stripTrailingZeros().toPlainString();
     }
 
     @Override
